4b165f131ad7e8db8f9a6e86b510abbbcd4ef04e
a.siver <a.siver@russianpost.ru>
2025-06-13 17:56:03 +0300

    clean up

--- processes/implementation/test.py
+++ processes/implementation/test.py
@@ -2,12 +2,12 @@ import json
 
 from processes.implementation.common import load_process_input
 
-PROCESS_TYPE = 'examples'
+PROCESS_TYPE = 'test'
 PROCESS_VERSION = 1
 
 
 def calc_input_id(process_input):
-    test_value = process_input['examples']
+    test_value = process_input['test']
     return f'{PROCESS_TYPE}.{PROCESS_VERSION}.{test_value}'
 
 
