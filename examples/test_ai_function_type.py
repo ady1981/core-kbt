fc18b68e8ae6eb60fcfc52445b3a5d8bfe5b319f
a.siver <a.siver@russianpost.ru>
2025-08-17 18:45:18 +0300

    add get_function_type with a test

---
+++ examples/test_ai_function_type.py
@@ -0,0 +1,5 @@
+from ai_function import get_function_type
+
+print(get_function_type('merge_items_by_semantics'))
+print(get_function_type('compare_things_by_aspects'))
+print(get_function_type('XXX'))
