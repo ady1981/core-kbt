5945d5cb063726009452f99568958c3668090ac5
a.siver <a.siver@russianpost.ru>
2025-09-28 18:06:15 +0300

    bugfix

--- examples/list_best_tasks_for_llm_effectivess.py
+++ examples/list_best_tasks_for_llm_effectivess.py
@@ -8,7 +8,7 @@ AI_FUN_NAME = 'list_best_tasks_for_llm_effectivess'
 
 
 def calc_instruction():
-    template_string = read_string(f'ai_functions/{AI_FUN_NAME}/prompt.md.j2')
+    template_string = read_string(f'ai_function_templates/{AI_FUN_NAME}/prompt.md.j2')
     data = {'context': 'Informational Technology',
             'tasks_range': '3-5',
             'task_area': 'SRE'}
@@ -19,7 +19,7 @@ model = os.environ["OPENAI_MODEL"]
 formatted_model_name = model.strip().replace("/", "-").replace(".", "-")
 instruction = calc_instruction()
 print(f'Instruction:\n{instruction}')
-response_schema = read_yaml(f'ai_functions/{AI_FUN_NAME}/output_schema.yaml')
+response_schema = read_yaml(f'ai_function_templates/{AI_FUN_NAME}/output_schema.yaml')
 response = evaluate2(instruction, response_schema, model=model)['json']
 print('Response:\n' + json.dumps(response, indent=2))
 write_json(response, f'temp/{AI_FUN_NAME}.{formatted_model_name}.response.json')
