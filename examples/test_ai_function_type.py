7f1efa0e0e349c40c431acb1b1704272f74565be
a.siver <a.siver@russianpost.ru>
2026-01-11 17:28:35 +0300

    clean up

--- examples/test_ai_function_type.py
+++ examples/test_ai_function_type.py
@@ -1,5 +1,5 @@
 from ai_function import calc_function_type
 
 print(calc_function_type('merge_items_by_semantics'))
-print(calc_function_type('compare_things_by_aspects'))
+print(calc_function_type('abstractive_summarize'))
 print(calc_function_type('XXX'))
