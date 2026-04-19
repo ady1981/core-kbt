2efcd4abfe98612718204ae40059b4ef551ae6d0
a.siver <a.siver@russianpost.ru>
2026-04-19 22:46:40 +0300

    add AI-functions for semantic merge

---
+++ examples/test_arrays_semantic_equivalence.py
@@ -0,0 +1,40 @@
+import json
+import json
+import os
+
+from ai_function_template import evaluate2
+from common import read_string, read_yaml, render_template, write_json, dump_yaml, format_markdown_code, read_json, \
+    parse_yaml, write_yaml
+
+AI_FUN_NAME = 'arrays_semantic_equivalence'
+
+
+def with_only_task_name(tasks):
+    return [{'task name': c['task name']} for c in tasks]
+
+
+def calc_instruction():
+    template_string = read_string(f'ai_function_templates/{AI_FUN_NAME}/prompt.md.j2')
+    array_1 = ['Code Generation', 'Code Explanation & Comprehension', 'Test Case Generation', 'Debugging and Error Resolution', 'Documentation Generation', 'Code Refactoring & Optimization']
+    array_2 = ['Code Generation', 'Code Debugging and Explanation', 'Documentation Generation', 'Test Case Generation', 'Code Refactoring', 'Requirements Analysis and Translation', 'Learning and Knowledge Acquisition']
+    array_item_json_schema = parse_yaml('''
+type: array
+items:
+  - type: string
+    description: a task
+''')
+    data = {'array_1': format_markdown_code('json', json.dumps(array_1, indent=2)),
+            'array_2': format_markdown_code('json', json.dumps(array_2, indent=2)),
+            'item_json_schema': array_item_json_schema}
+    return render_template(template_string, data)
+
+
+model = os.environ["OPENAI_MODEL"]
+formatted_model_name = model.strip().replace("/", "-").replace(".", "-")
+instruction = calc_instruction()
+print(f'instruction:\n{instruction}')
+response_schema = read_yaml(f'ai_function_templates/{AI_FUN_NAME}/output_schema.yaml')
+response = evaluate2(instruction, response_schema, model=model)
+json_response = response['json']
+print('=== Response:\n' + json.dumps(json_response, indent=2))
+write_yaml(json_response, f'temp/{AI_FUN_NAME}.{formatted_model_name}.response.yaml')
--- kbt-core/ai_function_impl/merge_items_by_semantics.py
+++ kbt-core/ai_function_impl/merge_items_by_semantics.py
@@ -1,25 +1,30 @@
 from functools import reduce
 
 import ai_function_template
-from common import first, rest, with_model_input_data
+from common import first, rest, with_model_input_data, log_str, dump_json
 
 
 def merge_arrays(model, left, right, array_item_json_schema, merge_strategy):
-    equivalence_items = ai_function_template.evaluate('estimate_arity2_array_criterial_semantic_equivalence', with_model_input_data({'array_1': left, 'array_2': right, 'item_json_schema': array_item_json_schema}, model))
+    equivalence_items = ai_function_template.evaluate('arrays_semantic_equivalence', with_model_input_data({'array_1': left, 'array_2': right, 'item_json_schema': array_item_json_schema}, model))
 
     upd_items = []
 
     # not_comparable
     not_comparable = equivalence_items.get('not_comparable', {})
+    ## TODO: outside log configuration
+    log_str('not_comparable:\n' + dump_json(not_comparable))
     upd_items += [c[1] for c in not_comparable.get('_1', [])]
     if merge_strategy.get('not_comparable') == 'merge':
         upd_items += rewrite_items_by_examples(model, [c[1] for c in not_comparable.get('_2', [])], left, array_item_json_schema)
 
     # comparable.equal
-    upd_items += [c['_1'][1] for c in equivalence_items.get('comparable', {}).get('equal', [])]
+    comparable_equal = equivalence_items.get('comparable', {}).get('equal', [])
+    log_str('comparable_equal:\n' + dump_json(comparable_equal))
+    upd_items += [c['_1'][1] for c in comparable_equal]
 
     # comparable.not_equal
     comparable_not_equal = equivalence_items.get('comparable', {}).get('not_equal', {})
+    log_str('comparable_not_equal:\n' + dump_json(comparable_not_equal))
     upd_items += [c[1] for c in comparable_not_equal.get('_1', [])]
     if merge_strategy.get('comparable_not_equal') == 'merge':
         upd_items += rewrite_items_by_examples(model, [c[1] for c in comparable_not_equal.get('_2', [])], left, array_item_json_schema)
@@ -51,10 +56,11 @@ def calc_context(array_item_json_schema):
 
 
 def rewrite_items_by_examples(model, items, example_items, array_item_json_schema):
-    context = calc_context(array_item_json_schema)
-    r = [ai_function_template.evaluate('rewrite_thing_by_examples', with_model_input_data({
-        'context': context,
+    item_description = calc_context(array_item_json_schema)
+    context_knowledge_specification = f'### Item description\n{item_description}'
+    r = [ai_function_template.evaluate('rewrite', with_model_input_data({
         'item': c,
+        'context_knowledge_specification': context_knowledge_specification,
         'examples': example_items
     }, model))['rewritten_item'] for c in items]
     return r
