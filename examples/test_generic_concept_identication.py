8d9b0450de889e72cf97ca75d17dfe3a0e1d5b52
a.siver <a.siver@russianpost.ru>
2026-04-21 21:13:08 +0300

    add extra ai_function_templates

---
+++ examples/test_generic_concept_identication.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+from ai_function_template import evaluate2
+from common import read_string, read_yaml, render_template, write_json, dump_yaml, format_markdown_code, write_yaml
+
+AI_FUN_NAME = 'generic_concept_identification'
+
+
+def calc_instruction():
+    concepts = '1. FastAPI\n2. Gin'
+    knowledge_domain = 'Information technology'
+    context_knowledge_specification = f'### Knowledge domain\n{knowledge_domain}'
+    data = {
+        'concepts': concepts,
+        'context_knowledge_specification': context_knowledge_specification
+    }
+    template_string = read_string(f'ai_function_templates/{AI_FUN_NAME}/prompt.md.j2')
+    return render_template(template_string, data)
+
+
+def main():
+    model = os.environ["OPENAI_MODEL"]
+    formatted_model_name = model.strip().replace("/", "-").replace(".", "-")
+    instruction = calc_instruction()
+    print(f'instruction:\n{instruction}')
+    response_schema = read_yaml(f'ai_function_templates/{AI_FUN_NAME}/output_schema.yaml')
+    response = evaluate2(instruction, response_schema, model=model)
+    json_response = response['json']
+    print('=== Response:\n' + json.dumps(json_response, indent=2))
+    # write_yaml(json_response, f'temp/{AI_FUN_NAME}.{formatted_model_name}.response.yaml')
+    write_json(json_response, f'temp/{AI_FUN_NAME}.{formatted_model_name}.response.json')
+
+main()
