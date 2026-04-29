d03a7c540fcb4b86b8ad5b55856d3e902ba41c27
a.siver <a.siver@russianpost.ru>
2026-04-29 22:46:08 +0300

    renaming

--- examples/test_essential_properties.py
+++ examples/test_essential_properties.py
@@ -4,7 +4,7 @@ import os
 from ai_function_template import evaluate2
 from common import read_string, read_yaml, render_template, write_json, dump_yaml, format_markdown_code, write_yaml
 
-AI_FUN_NAME = 'essensial_properties'
+AI_FUN_NAME = 'essential_properties'
 
 
 def calc_instruction():
