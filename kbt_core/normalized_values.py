305f9ee039204f35cc98e0117fc31bb98351f34e
a.siver <a.siver@russianpost.ru>
2026-05-10 14:04:47 +0300

    use default values and score in with_normalized_value

--- kbt_core/normalized_values.py
+++ kbt_core/normalized_values.py
@@ -1,5 +1,11 @@
+import os
+
+DEFAULT_VALUE = 0
+DEFAULT_SCORE = float(os.getenv('KBT_DEFAULT_SCORE', '1.0'))
+
+
 def with_normalized_value(items: list[dict], value_field: str, normalized_prefix: str, score_field: str, items_external_weight = 1.0):
-    total_score = sum(c[score_field] for c in items)
+    total_score = sum(c.get(score_field, DEFAULT_SCORE) for c in items)
     for c in items:
-        c[f'{normalized_prefix}{value_field}'] = c[value_field] * (c[score_field] / total_score) * items_external_weight
+        c[f'{normalized_prefix}{value_field}'] = c.get(value_field, DEFAULT_VALUE) * (c.get(score_field, DEFAULT_SCORE) / total_score) * items_external_weight
     return (total_score * items_external_weight, items)
