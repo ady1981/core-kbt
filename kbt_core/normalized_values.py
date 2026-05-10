2f0f6848342b9f7028c0e1616b62d9410224c037
a.siver <a.siver@russianpost.ru>
2026-05-10 14:17:15 +0300

    use default values and score in with_normalized_value

--- kbt_core/normalized_values.py
+++ kbt_core/normalized_values.py
@@ -1,7 +1,7 @@
 import os
 
 DEFAULT_VALUE = 0
-DEFAULT_SCORE = float(os.getenv('KBT_DEFAULT_SCORE', '1.0'))
+DEFAULT_SCORE = float(os.getenv('KBT_DEFAULT_SCORE')) if os.getenv('KBT_DEFAULT_SCORE', False) else 1.0
 
 
 def with_normalized_value(items: list[dict], value_field: str, normalized_prefix: str, score_field: str, items_external_weight = 1.0):
