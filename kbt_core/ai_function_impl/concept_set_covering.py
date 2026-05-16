43092a38f515b3ae2b5156836b3e8d865941d5c7
a.siver <a.siver@russianpost.ru>
2026-05-16 11:40:01 +0300

    add ensure_concept_relations_nonsymmetry check and etc

--- kbt_core/ai_function_impl/concept_set_covering.py
+++ kbt_core/ai_function_impl/concept_set_covering.py
@@ -6,9 +6,10 @@ OUTPUT_CONTENT_LANGUAGE = 'English'
 INFORMATION_RETRIEVAL_STRATEGY = 'Use unbiased internal knowledge'
 OUTPUT_GENERATION_STRATEGY = '''
 Extra_instructions:
-- write ONLY existing and correct relations
-- write empty relations if no relations
+- write ONLY existing and correct relations for ONLY specified entities
+- write empty relations if no existing and correct relations
 '''
+ISMEMBER_RELATIONS = ['subclassOf_schema', 'instanceOf_schema', 'partOf_schema']
 
 
 async def evaluate_via_process(func_name, input_data):
@@ -52,20 +53,21 @@ async def calc_perspective_concept_relations2(model, concept_nindex, concept_nam
 {basis_of_consideration}
 ### Perspective observer strategy
 {perspective_observer_strategy}
-### point_of_view
+### Point of view
 {point_of_view}
 '''
     input_data = with_model_input_data({
-        'a_concept': a_concept,
-        'b_concepts': b_concepts,
+        'a_concept': format_markdown_code('yaml', a_concept),
+        'b_concepts': format_markdown_code('yaml', ''.join(b_concepts)),
         'ontology_schema': format_markdown_code('yaml', ontology_schema),
         'meta_ontology': format_markdown_code('yaml', meta_ontology),
         'perspective': perspective2,
         '_information_retrieval_strategy': f'# Information retrieval strategy\n{INFORMATION_RETRIEVAL_STRATEGY}\n',
-        '_output_generation_strategy': f'# Output generation strategy\n{OUTPUT_GENERATION_STRATEGY}'
+        '_output_generation_strategy': f'# Output generation strategy\n{OUTPUT_GENERATION_STRATEGY}',
+        '_extra_output_specification': f'# Extra output specification\nOutput_content_language: {OUTPUT_CONTENT_LANGUAGE}'
     }, model)
     response = await evaluate_via_process('perspective_concept_relations', input_data)
-    return response["relation_items"]
+    return [relation for relation in response["relation_items"] if relation.get('is_correct')]
 
 
 async def calc_perspective_concept_relations(model, concepts, perspective):
@@ -80,13 +82,13 @@ def calc_concept_relations_map(concepts, perspective_concept_relations):
     for concept in concepts:
         by_subject = result.get(concept, {})
         for relation in perspective_concept_relations:
-            subject_entities = relation['subjectEntities']
+            subject_entities = relation['domainEntities']
             if isinstance(subject_entities, str):
                 subject_entities = [subject_entities]
-            if isinstance(relation['objectEntities'], str):
-                relation['objectEntities'] = [relation['objectEntities']]
+            if isinstance(relation['rangeEntities'], str):
+                relation['rangeEntities'] = [relation['rangeEntities']]
             if concept in subject_entities:
-                for object in relation['objectEntities']:
+                for object in relation['rangeEntities']:
                     by_object = by_subject.get(object, [])
                     by_object.append(relation)
                     by_subject[object] = by_object
@@ -95,43 +97,63 @@ def calc_concept_relations_map(concepts, perspective_concept_relations):
     return result
 
 
+def is_member_of(leftside_concept, rightside_concept, concept_relations_map):
+    return list_intersection(concept_relations_map
+                          .get(leftside_concept, {})
+                          .get(rightside_concept, {})
+                          .keys(),
+                          ISMEMBER_RELATIONS)
+
+
 def calc_concept_set_covering(concepts, concept_relations_map):
     concepts_n = len(concepts)
-    is_member_relations = ['subclassOf_schema', 'instanceOf_schema', 'partOf_schema']
     member_element_sets = [[subject_concept_idx
                             for subject_concept_idx in n_range(concepts_n)
-                              if subject_concept_idx == set_idx or
-                                list_intersection(concept_relations_map
-                                              .get(concepts[subject_concept_idx - 1], {})
-                                              .get(concepts[set_idx - 1], {})
-                                              .keys(),
-                                              is_member_relations)]
+                            if set_idx == subject_concept_idx or \
+                              is_member_of(concepts[subject_concept_idx - 1], concepts[set_idx - 1], concept_relations_map)]
                            for set_idx in n_range(concepts_n)]
-    print('member_element_sets:\n' + dump_json(member_element_sets)) ## TODO
+    # print('member_element_sets:\n' + dump_json(member_element_sets)) ## TODO
     set_covering_ids = calc_set_covering(concepts_n, member_element_sets)
     eliminated_ids = [idx for idx in n_range(concepts_n) if not idx in set_covering_ids]
     return (set_covering_ids, eliminated_ids)
 
 
+def ensure_concept_relations_nonsymmetry(concepts, concept_relations_map):
+    for c1 in concepts:
+        for c2 in concepts:
+            if c1 != c2:
+                if is_member_of(c1, c2, concept_relations_map) and is_member_of(c2, c1, concept_relations_map):
+                    log_str(f'invalid-concept_relations-symmetry: c1={c1}, c2={c2}, concept_relations_map=\n' + dump_json(concept_relations_map))
+                    raise RuntimeError('invalid-concept_relations-symmetry')
+
+
 async def evaluate(input_data):
-    meta = input_data.get('meta', {})
-    model = meta.get('model', None)
-    (observer_context_description, frame_of_reference, concepts) = (
-        input_data['observer_context_description'],
-        input_data['frame_of_reference'],
-        input_data['concepts']
-    )
-    perspective = await calc_perspective(model, observer_context_description, frame_of_reference)
-    perspective_concept_relations = await calc_perspective_concept_relations(model, concepts, perspective)
-    concept_relations_map = calc_concept_relations_map(concepts, perspective_concept_relations)
-    ## optimization
-    (set_covering_ids, eliminated_ids) = calc_concept_set_covering(concepts, concept_relations_map)
-    ## result
-    result = {
-        'covering_concepts': [concepts[c - 1] for c in set_covering_ids],
-        'eliminated_concepts': [{
-            'concept': concepts[c - 1],
-            'by_object_relations': concept_relations_map[concepts[c - 1]]
-        } for c in eliminated_ids]
-    }
-    return result
+    try:
+        meta = input_data.get('meta', {})
+        model = meta.get('model', None)
+        (observer_context_description, frame_of_reference, concepts) = (
+            input_data['observer_context_description'],
+            input_data['frame_of_reference'],
+            input_data['concepts']
+        )
+        perspective = await calc_perspective(model, observer_context_description, frame_of_reference)
+        perspective_concept_relations = await calc_perspective_concept_relations(model, concepts, perspective)
+        concept_relations_map = calc_concept_relations_map(concepts, perspective_concept_relations)
+        ## ensure consistency
+        ensure_concept_relations_nonsymmetry(concepts, concept_relations_map)
+        ## optimization
+        (set_covering_ids, eliminated_ids) = calc_concept_set_covering(concepts, concept_relations_map)
+        ## result
+        result = {
+            'covering_concepts': [concepts[c - 1] for c in set_covering_ids],
+            'eliminated_concepts': [{
+                'concept': concepts[c - 1],
+                'by_range_relations': concept_relations_map[concepts[c - 1]]
+            } for c in eliminated_ids]
+        }
+        return result
+    except RuntimeError as e:
+        if str(e) == 'invalid-concept_relations-symmetry':
+            return {'error': 'invalid-concept-relations-symmetry'}
+        else:
+            raise e
