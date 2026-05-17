4364784f2620a7ec768bf0aeca6b2f59b426fe25
a.siver <a.siver@russianpost.ru>
2026-05-17 14:08:14 +0300

    add extra attention to perspective

--- kbt_core/ai_function_impl/concept_set_covering.py
+++ kbt_core/ai_function_impl/concept_set_covering.py
@@ -7,6 +7,7 @@ OUTPUT_CONTENT_LANGUAGE = 'English'
 INFORMATION_RETRIEVAL_STRATEGY = 'Use unbiased internal knowledge'
 OUTPUT_GENERATION_STRATEGY = '''
 Extra_instructions:
+- give maximal attention to the Perspective and analyze the Ontology within the Perspective  
 - write ONLY existing and correct relations ('relation' kind) for ONLY specified entities ('entity' kind)
 - write empty relations if no correct relations
 '''
